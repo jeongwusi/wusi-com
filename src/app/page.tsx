19d1476c5c79bef236b5bba37cf1d38574beabc5
jeongwusi <jeongwusi@gmail.com>
2024-02-22 17:42:21 +0900

    ✨ feat: 메인 페이지 마크업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,28 @@
-import Link from "next/link";
 import Image from "next/image";
-import styles from "./page.module.css";
 import logo from "../../public/logo.svg";
+import styles from "./page.module.css";
 
-export default function Main() {
+export default function Home() {
   return (
     <main className={styles.container}>
-      <div className={styles.left}>
-        <Image src={logo} alt="logo" width={408} height={464} />
+      <div className={styles.contentContainer}>
+        <div className={styles.logo}>
+          <Image
+            src={logo}
+            alt="logo"
+            sizes="100dvw"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </div>
       </div>
-      <div className={styles.right}>
-        <h1>지금 일어나고 있는 일</h1>
-        <h2>지금 가입하세요.</h2>
-        <Link href="/i/flow/signup" className={styles.signup}>
-          계정 만들기
-        </Link>
-        <h3>이미 트위터에 가입하셨나요?</h3>
-        <Link href="/login" className={styles.login}>
-          로그인
-        </Link>
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          <span className={styles.mainCopy}>지금 일어나고 있는 일</span>
+          <span className={styles.subCopy}>지금 가입하세요.</span>
+          <button className={styles.joinButton}>계정 만들기</button>
+          <span className={styles.loginCopy}>이미 가입하셨나요?</span>
+          <button className={styles.loginButton}>로그인</button>
+        </div>
       </div>
     </main>
   );
